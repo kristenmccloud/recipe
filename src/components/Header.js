7c79f6983c5ab82b93272635796ee879604389f0
dlim <dlim87@gmail.com>
2018-11-06 11:49:35 -0800

    changing author

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,9 +3,11 @@ import React, { Component } from 'react';
 class Header extends Component {
   render() {
     return (
+
+      
       <div>
         <h1>KJ Brands Recipe App</h1>
-        
+
       </div>
     );
   }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,8 @@ import React, { Component } from 'react';
 class Home extends Component {
   render() {
     return (
+
+      
       <div>
         <h1>WELCOME HOMIES</h1>
         <p></p>
